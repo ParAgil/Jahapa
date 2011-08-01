db0ae577d6401d2c3a9dffa00a3f420f837318ca
Fito von Zastrow <adolfovon@gmail.com>
2011-07-31 18:46:18 -0700

    Location Name and Route Model

--- spec/models/location_name_spec.rb
+++ spec/models/location_name_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 describe 'Location Name' do
+  before do
+    @location = Location.create    
+    @location_name = LocationName.create :location_id => @location.id
+  end
   describe 'Relationships' do
-    before do
-      @location = Location.create
-      @location_name = LocationName.create :location_id => @location.id
-    end
-    
     it 'should have a location' do
       @location_name.location.should == @location
     end
+  end
+  it 'should have a name' do
+    @location_name.name = 'Shopping Mcal Lopez'
+    @location_name.name.should == 'Shopping Mcal Lopez'
   end
 end
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,18 +1,27 @@
 require 'spec_helper'
 describe 'Route' do
+  before do
+    @route = Route.create
+  end
   describe 'Relationships' do
     before do
-      @route = Route.create
       @location_1 = Location.create
+      @location_2 = Location.create
       @stop_1 = Stop.create :route_id => @route.id, :location_id => @location_1.id
+      @stop_2 = Stop.create :route_id => @route.id, :location_id => @location_2.id
     end
     
     it 'should have many stops' do
-      @route.stops.should == [@stop_1]
+      @route.stops.should == [@stop_1, @stop_2]
     end
     
     it 'should have many locations' do
-      @route.locations.should == [@location_1]
+      @route.locations.should == [@location_1, @location_2]
     end
+  end
+  
+  it 'should have a name' do
+    @route.name = 'Linea 49'
+    @route.name.should == 'Linea 49'
   end
 end
