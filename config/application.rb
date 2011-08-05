ba8134d9016dbfccba42a813b3ca9703a11d99a4
Fito von Zastrow <adolfovon@gmail.com>
2011-08-05 13:40:16 -0700

    Files movement and reorganzation as part of the update to Rails 3.1

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
@@ -38,5 +39,8 @@ module Jahapa
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # Enable the asset pipeline
+    config.assets.enabled = true
   end
 end
