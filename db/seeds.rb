dfb10b05accfc3624baf3d99bc22f63a3f0088ba
Fito von Zastrow <adolfovon@gmail.com>
2011-08-21 13:09:24 -0700

    seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-# ------------------ LOCATIONS -----------------------------------------------------#
+# ------------------------------------------------------ LOCATIONS ------------------------------------------------------------------------------#
 def make_location(hash)
   location = Location.create(:lat => hash[:lat], :long => hash[:long])
   names = hash[:names]
@@ -45,7 +45,7 @@ make_location(:lat => -25.316553, :long => -57.571170, :names => 'multiplaza,	sh
 
 make_location(:lat => -25.333592, :long => -57.520326, :names => 'una,	universidad nacional de asuncion,	facultad')
 
-make_location(:lat => -25.362796, :long => -57.563299,	:names => 'coca cola')
+make_location(:lat => -25.362796, :long => -57.563299, :names => 'coca cola')
 
 make_location(:lat => -25.326436, :long => -57.595988, :names => 'terminal,	terminal de asuncion,	terminal asuncion,	terminal de omnibus')
 
@@ -83,7 +83,54 @@ make_location(:lat => -25.289055, :long => -57.625266, :names => 'touring club, 
 
 make_location(:lat => -25.283683, :long => -57.629638, :names => 'estacion central del ferrocarril, estacion del ferrocarril, estacion de ferrocarril, estacion ferrocarril, ferrocarril')
 
-#-------------------------------- ROUTES -------------------------------------------#
+make_location(:lat => -25.295232, :long => -57.577742, :names => 'vox, vox casa central, casa central vox')
+
+make_location(:lat => -25.294449, :long => -57.579638, :names => 'claro villa morra')
+
+make_location(:lat => -25.263734, :long => -57.535465, :names => 'ñu guazu, ñu guasu')
+
+make_location(:lat => -25.280933, :long => -57.650172, :names => 'interpol')
+
+make_location(:lat => -25.280307, :long => -57.650132, :names => 'comisaria primera, comisaria 1ra')
+
+make_location(:lat => -25.279786, :long => -57.650703, :names => 'facultad de medicina, medicina una, facultad de medicina una, hospital de clinicas, clinicas')
+
+make_location(:lat => -25.287316, :long => -57.642984, :names => 'universidad autonoma, uaa')
+
+make_location(:lat => -25.298991, :long => -57.545139, :names => 'frigorifico guarani')
+
+make_location(:lat => -25.293183, :long => -57.549793, :names => 'cementerio del este')
+
+make_location(:lat => -25.251828, :long => -57.576942, :names => 'jardin botanico, botanico')
+
+make_location(:lat => -25.314817, :long => -57.583487, :names => 'jockey club paraguayo, jockey club')
+
+make_location(:lat => -25.315564, :long => -57.578305, :names => 'hipermercado luisito, luisito eusebio ayala')
+
+make_location(:lat => -25.317261, :long => -57.568445, :names => 'edesa')
+
+make_location(:lat => -25.321301, :long => -57.55704, :names => 'medalla milagrosa, iglesia medalla milagrosa, colegio medalla milagrosa')
+
+make_location(:lat => -25.323163, :long => -57.555053, :names => 'municipalidad fernando, municipalidad fernando de la mora, municipalidad de fernando')
+
+make_location(:lat => -25.324523, :long => -57.552532, :names => 'super el pueblo, supermercado el pueblo, super pueblo, supermercado pueblo')
+
+make_location(:lat => -25.325158, :long => -57.551357, :names => 'colegio san nicolas, colegio tecnico san nicolas')
+
+make_location(:lat => -25.334868, :long => -57.535202, :names => 'lasca, laboratorios lasca')
+
+make_location(:lat => -25.323493, :long => -57.526088, :names => 'andres barbero, instituto andres barbero, instituto dr andres barbero')
+
+make_location(:lat => -25.277897, :long => -57.64734, :names => 'casa rosada')
+
+make_location(:lat => -25.310402, :long => -57.530849, :names => 'dylan, dylan disco pub, discoteca dylan')
+
+make_location(:lat => -25.309633, :long => -57.531069, :names => 'chaco trading, chaco trading company')
+
+make_location(:lat => -25.300349, :long => -57.52866, :names => 'colegio tecnico san luis, tecnico san luis, colegio san luis')
+
+make_location(:lat => -25.228799, :long => -57.533695, :names => 'expo')
+#------------------------------------------------------------- ROUTES ---------------------------------------------------------------------------#
 
 def make_route( name, locations )
   locations = locations.is_a?(Array) ? locations : locations.split(',')
@@ -98,9 +145,24 @@ make_route('51', 'shopping del sol')
 
 make_route('15 - 3', '4 mojones, mercado de abasto, municipalidad, shopping villa morra,  shopping mariscal lopez, ministerio de industria y comercio,  iglesia de la recoleta,  embajada del japon, policlinico, shopping britanico, manzana t, identificaciones, embajada americana, tigo, plaza de las americas, mburuvicha roga, ministerio de defensa, embajada de brasil, teatro tom jobim, real acceso sur, touring club')
 
-make_route('28', 'hospital militar, tigo, supercentro, embajada americana')
+make_route('28', 'hospital militar, tigo, supercentro, embajada americana, vox, claro villa morra, shopping villa morra, shopping mcal lopez, shopping del sol, hospital policial, ñu guasu, interpol, comisaria primera, casa rosada')
 
-make_route('49 VUELTA', '4 mojones, mercado de abasto, multiplaza, una')
+make_route('49 VUELTA', '4 mojones, mercado de abasto, multiplaza, edesa, una')
+
+make_route('linea 5 (La Chaqueña)', 'facultad de medicina, universidad autonoma, expo')
+
+make_route('1 A', 'frigorifico guarani, cementerio del este, ñu guazu')
+
+make_route('1 B', 'frigorifico guarani, cementerio del este, jardin botanico')
+
+make_route('29', 'jockey club paraguayo, hipermercado luisito, multiplaza, edesa, medalla milagrosa, municipalidad fernando, supermercado pueblo, colegio san nicolas, real fernando, lasca, una')
+
+make_route('27', 'jockey club paraguayo, hipermercado luisito, multiplaza, edesa, medalla milagrosa, municipalidad fernando, supermercado pueblo, colegio san nicolas, real fernando, lasca, una')
+
+make_route('21 IPVU', 'multiplaza, edesa, medalla milagrosa, dylan, chaco trading, colegio tecnico san luis')
+
+
+
 
 
 
