e42a105c34cecbb73501bd5e9355ccdf2af19c46
Fito <fito@paragil.com>
2012-02-25 11:32:06 -0800

    getting the app ready for facebook

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,4 +6,7 @@ class HomeController < ApplicationController
       @routes = []
     end
   end
+  
+  def facebook
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Jahapa::Application.routes.draw do
 
   resources :locations
   
+  match '/facebook' => 'home#facebook'
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
