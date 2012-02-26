7ab4c850cd386fe2831ed933e7b84376af093752
Fito <fito@paragil.com>
2012-02-26 11:46:52 -0800

    Especial layout for facebook added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,5 +8,14 @@ class HomeController < ApplicationController
   end
   
   def facebook
+    if params[:start_location] && params[:end_location] 
+      @routes = Route.from_locations( params )
+    else
+      @routes = []
+    end
+    
+    respond_to do |format|
+      format.html { render 'home/facebook', :layout => 'facebook_layout'}
+    end
   end
 end
